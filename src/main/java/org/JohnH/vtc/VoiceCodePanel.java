a7ece7966884b417ce0c3c3d44d57dfc5731f87f
Rdcat <jhunt3@guilford.edu>
2025-11-11 09:32:47 -0500

    Start commit to make a repositoriry for this

---
+++ src/main/java/org/JohnH/vtc/VoiceCodePanel.java
@@ -0,0 +1,19 @@
+package org.JohnH.vtc;
+
+/**
+ * VoiceCodePanel will be the main panel for voice code interactions.
+ */
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+public class VoiceCodePanel extends JPanel {
+    JLabel recordLabel = new JLabel("Record your voice code:");
+    JButton recordButton = new JButton("Record");
+
+    
+
+    
+    public VoiceCodePanel() {
+        // TODO: implement panel functionality
+    }
+}
---
+++ src/main/java/org/JohnH/vtc/Vtc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.JohnH.vtc;
+
+/**
+ *
+ * @author johnhunt
+ */
+
+public class Vtc {
+
+    public static void main(String[] args) {
+    }
+}
