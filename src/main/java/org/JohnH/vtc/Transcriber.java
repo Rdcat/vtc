7590af4077b23b579c4cd21e29c83eac8eb20c81
Rdcat <jhunt3@guilford.edu>
2025-11-11 09:32:47 -0500

    Start commit to make a repositoriry for this

---
+++ src/main/java/org/JohnH/vtc/Transcriber.java
@@ -0,0 +1,23 @@
+package org.JohnH.vtc;
+
+/**
+ * this is the back end that will have the methods to listen, transcribe, and translate to code
+ */
+
+public class Transcriber{
+
+    //Start Recording Function
+    public void StartRecording(){
+        //start recording logic
+    }
+    //Stop recording Function
+    public void StopRecording(){
+        //stop recording logic
+    }
+    //writing the audio out
+    public void AudioToText(){
+        //Audio converter logic
+    }
+    
+
+}
